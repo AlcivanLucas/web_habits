14039aa168470546082854eb32e57315c82a1577
Alcivan Lucas <61625442+GalinGdS@users.noreply.github.com>
2023-08-04 16:42:29 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Rotas } from './Rotas.tsx'
+import  Rotas  from './Rotas'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
