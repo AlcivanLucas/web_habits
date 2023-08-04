4806e2e6fff8352892ddd5113bfa28ca90a11e0e
Alcivan Lucas <61625442+GalinGdS@users.noreply.github.com>
2023-08-04 16:41:22 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './pages/App'
+import { Rotas } from './Rotas.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Rotas />
   </React.StrictMode>,
 )
