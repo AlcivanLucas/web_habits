b819ffce994acb5cc53f23254151539a2123fab5
Alcivan Lucas <61625442+GalinGdS@users.noreply.github.com>
2023-08-05 10:03:43 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import  Login  from './pages/Login'
-
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
