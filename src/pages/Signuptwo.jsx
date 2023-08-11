0868f95164c9e6a255412c231943b32895a77675
Alcivan Lucas <61625442+GalinGdS@users.noreply.github.com>
2023-08-11 10:19:59 -0300

    add rotas alternativas

--- src/pages/Signuptwo.jsx
+++ src/pages/Signuptwo.jsx
@@ -1,40 +1,43 @@
-import React from 'react'
+
 import loginImg from '../assets/login.jpg'
 
-import { useState } from "react";
+import  React, { useState } from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 
-
 export default function Signuptwo() {
 
-    const { signin } = useAuth();
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [emailConf, setEmailConf] = useState("");
+  const [senha, setSenha] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const { signup } = useAuth();
   
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-    const [error, setError] = useState("");
+  const handleSignup = () => {
+    if (!email || !emailConf || !senha) {
+      setError("Preencha todos os campos");
+      return;
+    } else if (email !== emailConf) {
+      setError("Os e-mails não são iguais");
+      return;
+    }
+
+    const res = signup(email, senha);
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    alert("Usuário cadatrado com sucesso!");
+    navigate("/");
+  };
   
-    const handleLogin = () => {
-      if (!email || !senha) {
-        setError("Preencha todos os campos");
-        return;
-      }
-  
-      const res = signin(email, senha);
-  
-      if (res) {
-        setError(res);
-        return;
-      }
-  
-      navigate("/home");
-    };
-  
-    
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
         <div className='hidden sm:block'>
@@ -56,6 +59,16 @@ export default function Signuptwo() {
                 </div>
 
                 <div className='flex flex-col text-gray-400 py-2'>
+                    <label>Confirm Username</label>
+                    <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' 
+                        type="email"
+                        placeholder="Confirm seu E-mail"
+                        value={emailConf}
+                        onChange={(e) => [setEmailConf(e.target.value), setError("")]}
+                         />
+                </div>
+
+                <div className='flex flex-col text-gray-400 py-2'>
                     <label>Password</label>
                     <input className='p-2 rounded-lg bg-gray-700 mt-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' 
                         type="password"
@@ -65,23 +78,20 @@ export default function Signuptwo() {
                         />
                 </div>
 
-                <div className='flex flex-col text-gray-400 py-2'>
-                    <label>Confirm Password</label>
-                    <input className='p-2 rounded-lg bg-gray-700 mt-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' 
-                        type="password"
-                        placeholder="Digite sua Senha"
-                        value={senha}
-                        />
-                </div>
+               
+
                 
                 <div className='flex justify-between text-gray-400 py-2'>
                     <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
-                    <Link to="/">&nbsp;Entrar</Link>
+                    <Link to="/">&nbsp;Entre</Link>
                     
                     
                 </div>
 
-                <button className='w-full my-5 py-2 bg-violet-700 shadow-lg shadow-violet-500/50 hover:shadow-violet-500/40 text-white font-semibold rounded-lg' onClick={handleLogin}    >SIGNIN</button>
+                <button className='w-full my-5 py-2 bg-violet-700 shadow-lg shadow-violet-500/50 hover:shadow-violet-500/40 text-white font-semibold rounded-lg' 
+                  onClick={handleSignup}  >
+                    SIGNUP
+                </button>
                 
             </form>
         </div>
